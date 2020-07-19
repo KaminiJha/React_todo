7f247855c72a88b1075f14fe164efa20df16f409
Kamini Jha <kamini123skt@gmail.com>
2020-07-19 17:26:12 +0530

    Updates

---
+++ precache-manifest.27803cd05b220b65edb79bf0497fe305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811e1f59258f312e9998eca66c564da0",
+    "url": "/React_todo/index.html"
+  },
+  {
+    "revision": "13e1ec2e60dab679b44c",
+    "url": "/React_todo/static/css/main.343edef1.chunk.css"
+  },
+  {
+    "revision": "21e5b637f3bd35d39a2b",
+    "url": "/React_todo/static/js/2.ccf2fd30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_todo/static/js/2.ccf2fd30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e1ec2e60dab679b44c",
+    "url": "/React_todo/static/js/main.7be794fd.chunk.js"
+  },
+  {
+    "revision": "0bd568241a10034883c0",
+    "url": "/React_todo/static/js/runtime-main.0dc15684.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
